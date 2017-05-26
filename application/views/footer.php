bbe45a9b6c70c3711e176fb416b95123d8cdca7c
fmedina <katerinne0490@gmail.com>
2017-05-26 12:12:17 -0400

    cambios footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
 			<div class="pull-right">
 			<!-- 10GB of <strong>250GB</strong> Free.-->
 			</div>
-			<div><strong>Copyright</strong> Exampley Company &copy; 2017</div>
+			<div><strong></strong> Lubricar Delivery &copy; 2017</div>
 		</div>
 	</div>
 </div>
