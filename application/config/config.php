7ae08aeb7021f64bd76d0a5972444360aa2de360
Jose <solorzano202009@gmail.com>
2017-05-30 16:41:25 -0400

    Ajustes al base_url y la expiración de sesión en config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://10.9.35.152/lubricar-web/';
+$config['base_url'] = 'http://192.168.1.132/lubricar-web/';
 
 /*
 |--------------------------------------------------------------------------
@@ -382,7 +382,7 @@ $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
+$config['sess_time_to_update'] = 1800;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
